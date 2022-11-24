7cf4e8ca59f338dbe3d7ce78da6ed8517480386a
Gabriel-Teodoro <gabrieltodoro1@hotmail.com>
2022-11-23 23:30:06 -0300

    aula15 - aprendendo api

--- js/index.js
+++ js/index.js
@@ -1,22 +1,36 @@
 // Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
-fetch('https://randomuser.me/api/')
+// fetch('https://randomuser.me/api/')
+//     .then(response => {
+//         return response.json()
+//     })
+//     .then(data => {
+//         renderizarDadosUsuario(data)
+//         console.log(data)
+//     });
+
+    /* --------------------------- Tarefa 1 ------------------------------- */
+function renderizarDadosUsuario(dados) {
+    let elementoPai = document.querySelector(".card")
+        let user = dados.results[0];
+        let cardElements = `
+        <img src="${user.picture.large}" alt="foto de usuario">
+        <h2>${user.name.first} ${user.name.last}</h2>
+        <h3>e-mail: ${user.email}</h3>
+        <h4>senha: ${user.login.password}</h3>
+        `
+        elementoPai.innerHTML = cardElements
+}
+
+/* --------------------------- Tarefa 2 (extra) --------------------------- */
+btnRandom = document.getElementById('random');
+btnRandom.addEventListener('click', evento => {
+    evento.preventDefault();
+    fetch('https://randomuser.me/api/')
     .then(response => {
         return response.json()
     })
     .then(data => {
-        //manipulamos a resposta
+        renderizarDadosUsuario(data)
         console.log(data)
     });
-
-function renderizarDadosUsuario(dados) {
-    /* -------------------------------- Tarefa 1 -------------------------------- */
-    // Aqui devem desenvolver uma função que seja exibida na tela:
-    // a foto, o nome completo do usuário e o e-mail.
-    // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
-}
-
-
-/* --------------------------- Tarefa 2 (extra) --------------------------- */
-// Aqui você pode ir para o ponto extra de usar o botão que está comentado no HTML.
-// Você pode descomentar o código no index.html e usar esse botão para executar uma nova solicitação API, sem recarregar a página.
-// Cabe aos desenvolvedores decidirem qual bloco de código deve ser contido dentro de uma função para que ele possa ser executado toda vez que um clique de botão for realizado.
+})
